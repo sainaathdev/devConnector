f02ba95a0fbedbf7ee406688dac28de372d1343c
sainaathdev <133358924+sainaathdev@users.noreply.github.com>
2024-04-29 14:04:06 +0530

    github and posts

--- routes/apis/posts.js
+++ routes/apis/posts.js
@@ -1,10 +1,77 @@
-const express  = require("express")
+const express  = require("express");
+const auth = require("../../middleware/auth");
 const router = express.Router()
+const { check, validationResult } = require('express-validator');
+const User = require("../../models/User.model");
+const Post = require("../../models/Posts.model");
 
-//@user  GET api/posts
-//@desc  test route
-//@access public
+// @route    POST api/posts
+// @desc     Create a post
+// @access   Private
 
-router.get('/', (req,res) => res.send("Posts route"))
+router.post('/', [ auth, [
+    check('text', "Text is required").not().isEmpty()
+]] , async (req,res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+    
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        });
+    
+        const post = await newPost.save();
+    
+        res.json(post)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
+
+
+// @route    GET api/posts
+// @desc     Get all posts
+// @access   Private
+
+router.get('/', auth, async (req, res) => {
+    try {
+      const posts = await Post.find().sort({ date: -1 });
+      res.json(posts);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
+
+
+// @route    GET api/posts/:id
+// @desc     Get post by ID
+// @access   Private
+
+router.get('/:id', auth, async (req, res) => {
+    try {
+      const posts = await Post.findById(req.params.id)
+      if(!posts){
+        return res.status(404).json({ msg: 'Post not found' });
+      }
+      res.json(posts);
+    } catch (err) {
+      console.error(err.message);
+      if (err.kind === 'ObjectId'){
+        return res.status(404).json({ msg: 'post not found3' });
+      }
+      
+      res.status(500).send('Server Error');
+    }
+  });
+
 
 module.exports = router;
--- routes/apis/profile.js
+++ routes/apis/profile.js
@@ -1,4 +1,6 @@
 const express  = require("express");
+const axios = require('axios');
+const config = require('config');
 const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile.model");
 const User = require("../../models/User.model");
@@ -329,6 +331,29 @@ router.delete('/education/:edu_id', auth, async (req,res) => {
     }
 })
 
+
+// @route    GET api/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
+router.get('/github/:username', async (req, res) => {
+    try {
+      const uri = encodeURI(
+        `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+      );
+      const headers = {
+        'user-agent': 'node.js',
+        Authorization: `token ${config.get('githubAcessToken')}`
+      };
+  
+      const gitHubResponse = await axios.get(uri, { headers });
+      return res.json(gitHubResponse.data);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(404).json({ msg: 'No Github profile found' });
+    }
+  });
+    
+    
 
 
 
